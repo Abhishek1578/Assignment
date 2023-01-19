b8a3c2618b4fb6d908df1cbb233c5e64ec0819c4
LENOVO <LENOVO@LAPTOP-3QDC6DUQ.mshome.net>
2023-01-19 14:38:59 +0530

    hello java

---
+++ leapyear/src/leapyear/leapyear.java
@@ -0,0 +1,157 @@
+package leapyear;
+import java.util.Scanner;
+public class leapyear {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Scanner s=new Scanner(System.in);
+//		System.out.print("Please choose your operator ");
+//		String z=s.nextLine();
+//		System.out.print("Please enter first number ");
+//		int x=s.nextInt();
+//		System.out.print("Please enter second number ");
+//		int y=s.nextInt();
+//		
+//		if(x>75 && x<=100)
+//		{
+//			System.out.println("U are in first class ");
+//		}
+//		else if(x<75 && x>=50)
+//		{
+//			System.out.println("u are in second class ");
+//		}
+//		else if(x<50 && x>=30)
+//		{
+//			System.out.println("u are just passed in the exam ");
+//		}
+//		else if(x>100)
+//		{
+//			System.out.println("please enter number between 1 to 100");
+//		}
+//		else
+//		{
+//			System.out.println("U are badly failed in your exam");
+//		}
+//		if(x<10)
+//		{
+//			System.out.println("Good morning");
+//		}
+//		else if(x<20)
+//		{
+//			System.out.println("Good day");
+//		}
+//		else
+//		{
+//			System.out.println("good evening ");
+//		}
+//		if(x<10)
+//		{
+//			int st;
+//			System.out.println(st=x+2);
+//		}
+//		else if(x>=10)
+//		{
+//			int stt;
+//			System.out.println(stt=x*2+2);
+//		}
+//		else
+//		{
+//			System.out.println("please enter valid number");
+//		}
+		//different
+		
+//		if(x==3)
+//		{
+//			System.out.println("this is traiangle");
+//			
+//		}else if(x==4)
+//		{
+//			System.out.println("this is quadilateral triangle");
+//			
+//		}
+//		else if(x==5)
+//		{
+//			System.out.println("this is pentagonal");
+//		}
+//		else
+//		{
+//		
+//			System.out.println("'invalid number");
+//			
+//		}
+		
+		
+		
+		
+		//chess proble 
+
+//		if(((x==1)||(x==3)||(x==5)||(x==7))&&((y==1)||(y==3)||(y==5)||(y==7)))
+//		{
+//			System.out.println("u are in black square");
+//		}
+//		else if(((x==2)||(x==4)||(x==6)||(x==8))&&((y==2)||(y==4)||(y==6)||(y==8)))
+//		{
+//			System.out.println("u are in black square");
+//		}
+//		else 
+//		{
+//			System.out.println("u are in white square");
+//		}
+		
+		
+		
+		
+		//String in java 
+//		if(x.length()>7)
+//		{
+//			System.out.println(x);
+//		}
+//		else
+//		{
+//			System.out.println("Invalid string ");
+//		}
+		//both way is good 
+//		String i=(x.length()>7)? x:"invalid";
+//		System.out.println(i);
+		
+		
+		
+		//String in java 
+		
+//		String greet="hello";
+//		String str2="hello";
+//		String str1=new String("hello");
+//		String s=str1.concat("hello");
+//		System.out.println(s);
+		
+		
+		
+//		if(z.equals("+"))
+//		{
+//			System.out.println("addition is "+x+y);
+//		}
+//		else if(z.equals("-"))
+//		{
+//			System.out.println(x-y);
+//		}
+//		else if(z.equals("*"))
+//		{
+//			System.out.println("Multiplication is "+x*y);
+//		}
+//		else if(z.equals("/"))
+//		{
+//			System.out.println("Division is "+x/y);
+//		}
+//		else
+//		{
+//			System.out.print("please enter valid operator");
+//		}
+		String s1="tutorial";
+		String b="tutorial";
+		String c="method";
+		System.out.println(s1.compareTo(b));
+		System.out.println("");
+		}
+	}
+
+
