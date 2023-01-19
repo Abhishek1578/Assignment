e4e1bbc6145e923396b2dae28759f5c5aee124fb
LENOVO <LENOVO@LAPTOP-3QDC6DUQ.mshome.net>
2023-01-19 16:30:03 +0530

    hello java

---
+++ assignement/src/assignement/hi.java
@@ -0,0 +1,10 @@
+package assignement;
+
+public class hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello");
+	}
+
+}
---
+++ project/src/project/guit.java
@@ -0,0 +1,11 @@
+package project;
+
+public class guit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello sir");
+
+	}
+
+}
